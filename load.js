64436e80ee95a7cb017c735e8c152852ffba62a5
jaximuslim <jaxlimhk@gmail.com>
2022-04-08 16:04:22 +0800

    1.1.18

--- load.js
+++ load.js
@@ -1,3 +1,3 @@
 QuintOS.username = 'jaximuslim'; // put your github username!
 QuintOS.dir = 'GAMES'; // the folder with the game folder in it
-QuintOS.gameTitle = 'Snake'; // the title of the game to load
+QuintOS.game = 'Snake'; // the title of the game to load
