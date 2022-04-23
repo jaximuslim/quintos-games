84f45fb15dc40419511f99557515f1a8ede8f776
jaximuslim <jaxlimhk@gmail.com>
2022-04-23 17:04:48 +0800

    1.1.21

--- games_java/Wordle/Wordle.java
+++ games_java/Wordle/Wordle.java
@@ -6,12 +6,14 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.Collections;
 
 public class Wordle {
 	ArrayList<String> dictionary;
 	ArrayList<String> words;
 	String[] board;
 	boolean gameOver = false;
+	String rectType = "solid";
 
 	public Wordle() {
 		setupGame();
@@ -29,11 +31,11 @@ public class Wordle {
 			return;
 		}
 		while (fileScanner.hasNextLine()) {
-	  	words.add(fileScanner.nextLine());
-    }
+			words.add(fileScanner.nextLine());
+		}
 		log(words);
-    fileScanner.close();
-		
+		fileScanner.close();
+
 		dictionary = new ArrayList<String>();
 		File dict_file = new File(dir + "/dictionary5.txt");
 		try {
@@ -42,48 +44,57 @@ public class Wordle {
 			return;
 		}
 		while (fileScanner.hasNextLine()) {
-	  	String[] arr = (fileScanner.nextLine()).split(" ");
-			for (int i = 0; i < arr.length; i++){
+			String[] arr = (fileScanner.nextLine()).split(" ");
+			for (int i = 0; i < arr.length; i++) {
 				dictionary.add(arr[i]);
 			}
-    }
-		log(dictionary.get(0));
-    fileScanner.close();
+		}
+
+		fileScanner.close();
 		startGame();
 	}
+
 	/* Display all the boxes for the letters */
 	void displayBoxes() {
-		for(int row = 1; row <6; row++){
-			for(int col = 1; col<6;col++){
-				textRect(row*3,col*3,3,3);
+		for (int row = 1; row < 7; row++) {
+			for (int col = 1; col < 6; col++) {
+				textRect(row * 3, col * 3, 3, 3);
 			}
 		}
 	}
 
 	void startGame() {
 		/* pick new word */
+
 		int validGuesses = 0;
-		int randomWordIndex = (int) (Math.floor(Math.random()*words.size()));
+		int randomWordIndex = (int) (Math.floor(Math.random() * words.size()));
 		String word = words.get(randomWordIndex);
 		log(word);
 
 		displayBoxes();
 
-		while (gameOver == false){
+		while (gameOver == false) {
 			String guess = (prompt("Guess the word!", 3, 18, 20)).toUpperCase();
-			if(guess.length() != 5){
-				alert("Only 5 letter words!",15,5,9);
-				displayBoxes();
-			}
-			else{
+			if (guess.length() != 5) {
+				alert("Only 5 letter words!", 3, 18, 20);
+			} else {
 				String[] word_alphabets = word.split("");
 				String[] guess_alphabets = guess.split("");
 				log(guess_alphabets);
-				for(int i = 0; i< guess_alphabets.length();i++){
-					text(guess_alphabets[i],4+validGuesses*3,4+i*3);
+
+				log(word_alphabets);
+				for (int i = 0; i < guess_alphabets.length(); i++) {
+					if (word_alphabets[i] == guess_alphabets[i]) {
+						eraseRect(3 + validGuesses * 3, 3 + i * 3, 3, 3);
+						textRect(3 + validGuesses * 3, 3 + i * 3, 3, 3, "dashed");
+					} else if (word.contains(guess_alphabets[i])) {
+						eraseRect(3 + validGuesses * 3, 3 + i * 3, 3, 3);
+						textRect(3 + validGuesses * 3, 3 + i * 3, 3, 3, "outline");
+					}
+					text(guess_alphabets[i], 4 + validGuesses * 3, 4 + i * 3);
+
 				}
 				validGuesses++;
-				log(word_alphabets);
 			}
 		}
 	}
